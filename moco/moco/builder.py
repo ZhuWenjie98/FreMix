37e6f2572cd021b7f693378a789eb9501c9d4da6
root <wz2140@nyu.edu>
2022-09-25 04:49:46 +0000

    update fremix

--- moco/moco/builder.py
+++ moco/moco/builder.py
@@ -75,22 +75,23 @@ def colorful_spectrum_mix(img1, img2, alpha, ratio=1.0):
     lam = np.random.uniform(0, alpha)
 
     assert img1.shape == img2.shape
-    h, w, c = img1.shape
+    b, h, w, c = img1.shape
+    ratio = torch.tensor(ratio, dtype=torch.int8)
     h_crop = int(h * torch.sqrt(ratio))
     w_crop = int(w * torch.sqrt(ratio))
     h_start = h // 2 - h_crop // 2
     w_start = w // 2 - w_crop // 2
 
-    img1_fft = torch.fft.fft2(img1, axes=(0, 1))
-    img2_fft = torch.fft.fft2(img2, axes=(0, 1))
+    img1_fft = torch.fft.fft2(img1, dim=(1,2))
+    img2_fft = torch.fft.fft2(img2, dim=(1,2))
     img1_abs, img1_pha = torch.abs(img1_fft), torch.angle(img1_fft)
     img2_abs, img2_pha = torch.abs(img2_fft), torch.angle(img2_fft)
 
-    img1_abs = torch.fft.fftshift(img1_abs, axes=(0, 1))
-    img2_abs = torch.fft.fftshift(img2_abs, axes=(0, 1))
+    img1_abs = torch.fft.fftshift(img1_abs, dim=(1, 2))
+    img2_abs = torch.fft.fftshift(img2_abs, dim=(1, 2))
 
-    img1_abs_ = torch.copy(img1_abs)
-    img2_abs_ = torch.copy(img2_abs)
+    img1_abs_ = torch.clone(img1_abs)
+    img2_abs_ = torch.clone(img2_abs)
     ## phase1 + lam * amp2 + (1-lam) * amp1
     img1_abs[h_start:h_start + h_crop, w_start:w_start + w_crop] = \
         lam * img2_abs_[h_start:h_start + h_crop, w_start:w_start + w_crop] + (1 - lam) * img1_abs_[
@@ -101,14 +102,14 @@ def colorful_spectrum_mix(img1, img2, alpha, ratio=1.0):
         lam * img1_abs_[h_start:h_start + h_crop, w_start:w_start + w_crop] + (1 - lam) * img2_abs_[
                                                                                           h_start:h_start + h_crop,
                                                                                           w_start:w_start + w_crop]
-    img1_abs = torch.fft.ifftshift(img1_abs, axes=(0, 1))
-    img2_abs = torch.fft.ifftshift(img2_abs, axes=(0, 1))
+    img1_abs = torch.fft.ifftshift(img1_abs, dim=(1, 2))
+    img2_abs = torch.fft.ifftshift(img2_abs, dim=(1, 2))
     img21 = img1_abs * (np.e ** (1j * img1_pha)) 
     img12 = img2_abs * (np.e ** (1j * img2_pha))
-    img21 = torch.real(torch.fft.ifft2(img21, axes=(0, 1)))
-    img12 = torch.real(torch.fft.ifft2(img12, axes=(0, 1)))
-    img21 = torch.uint8(torch.clip(img21, 0, 255))
-    img12 = torch.uint8(torch.clip(img12, 0, 255))
+    img21 = torch.real(torch.fft.ifft2(img21,dim=(1, 2)))
+    img12 = torch.real(torch.fft.ifft2(img2,dim=(1, 2)))
+    img21 = torch.clip(img21[0], 0, 255).int()
+    img12 = torch.clip(img12[0], 0, 255).int()
 
     return img21, img12
 
